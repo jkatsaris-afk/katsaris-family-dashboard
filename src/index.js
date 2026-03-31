17f37b1b68d94e7b5c4f1849d9cc6f581e90e3cd
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-03-31 06:12:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+@keyframes bounce {
+  0%, 80%, 100% {
+    transform: scale(0);
+  }
+  40% {
+    transform: scale(1);
+  }
+}
