3f7e2e23fd392fd8d45031ca5d6ff173cc755076
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-03-31 11:35:29 -0700

    Create WeatherPage.js

---
+++ src/WeatherPage.js
@@ -0,0 +1,7 @@
+export default function WeatherPage() {
+  return (
+    <div style={{ background: "#fff", padding: "20px", borderRadius: "20px" }}>
+      Weather page coming soon...
+    </div>
+  );
+}
