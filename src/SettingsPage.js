74f8be0e0822909b2c159ecefb8c3d3ab75efa0e
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-03-31 11:35:49 -0700

    Create SettingsPage.js

---
+++ src/SettingsPage.js
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+  return (
+    <div style={{ background: "#fff", padding: "20px", borderRadius: "20px" }}>
+      Settings page coming soon...
+    </div>
+  );
+}
