25666b9ffa984b778948b06bd58fd58405116219
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-01 12:22:18 -0700

    Create FamilyPage.js

---
+++ src/FamilyPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FamilyPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Family</h2>
+      <p>This is where family members will go.</p>
+    </div>
+  );
+}
