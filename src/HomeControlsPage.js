113ac374bfa506deb86c5521a56ea15b038651e6
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-01 12:22:44 -0700

    Create HomeControlsPage.js

---
+++ src/HomeControlsPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomeControlsPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Home Controls</h2>
+      <p>This is where smart home controls will go.</p>
+    </div>
+  );
+}
