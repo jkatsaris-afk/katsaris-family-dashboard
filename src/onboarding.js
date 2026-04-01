148a6465e705adf047d1ff96faa1217510c3bae9
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-01 10:59:18 -0700

    Update onboarding.js

--- src/onboarding.js
+++ src/onboarding.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../supabaseClient";
+import { supabase } from "./supabaseClient";
 import logo from "../assets/oikos-brand.png";
 
 export default function OnboardingPage() {
